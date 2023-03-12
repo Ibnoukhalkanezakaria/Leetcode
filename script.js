60d9cdfc580a275e67ac1b234c9efc268014c659
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-03-12 02:04:00 +0100

    new

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // https://leetcode.com/problems/plus-one/
-var plusOne = function (digits) {
-  let last = [];
-  for (let i = 0; i < digits.length; i++) {
-    if (digits.length > 1) {
-      last.push(+digits.slice(-1) + 1);
-      return digits.slice(0, length - 1).concat(last);
-    } else {
-      return (+digits + 1).toString().split("").map(Number);
-    }
-  }
-};
 
-console.log(plusOne([1, 2, 3]));
-console.log(plusOne([4, 3, 2, 1]));
-console.log(plusOne([9]));
+// https:leetcode.com/problems/valid-parentheses/
+
+// https:leetcode.com/problems/single-number/
