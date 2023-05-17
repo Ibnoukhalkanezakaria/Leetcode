c5c1a003b169f3117f2ccdf0b38d86be613ad7e8
Ibnoukhalkanezakaria <Ibnoukhalkanezakaria@gmail.com>
2023-05-17 19:04:28 +0100

    f

--- script.js
+++ script.js
@@ -63,18 +63,18 @@
 
 //https:leetcode.com/problems/remove-duplicates-from-sorted-array/
 
-var removeDuplicates = function (nums) {
-  if (nums.length === 0) {
-    return 0;
-  }
-  let index = 1;
+// var removeDuplicates = function (nums) {
+//   if (nums.length === 0) {
+//     return 0;
+//   }
+//   let index = 1;
 
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] !== nums[i - 1]) {
-      nums[index] = nums[i];
-      index++;
-    }
-  }
+//   for (let i = 1; i < nums.length; i++) {
+//     if (nums[i] !== nums[i - 1]) {
+//       nums[index] = nums[i];
+//       index++;
+//     }
+//   }
 
-  return index;
-};
+//   return index;
+// };
